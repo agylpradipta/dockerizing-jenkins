d2a4e27393897a29fc8271b0aac1b7165b556af5
agylpradipta <agylpradipta70@gmail.com>
2021-11-07 17:37:33 +0700

    setup elastic

--- runall.sh
+++ runall.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#Build filebeatimage
+docker build -t starlord345/filebeatimage:latest . -f filebeat_docker/Dockerfile
+
 #clean anything with same name to get rid of clashes
 docker-compose down
 docker-compose -f docker-compose-elk.yml down
