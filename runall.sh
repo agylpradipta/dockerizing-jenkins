1039e6d6c3e8f9a46b45a1dbd04876a7bf01be07
agylpradipta <agylpradipta70@gmail.com>
2021-11-08 09:48:52 +0700

    add jenkins container

--- runall.sh
+++ runall.sh
@@ -7,14 +7,17 @@ docker build -t filebeatimage .
 #clean anything with same name to get rid of clashes
 cd ../docker_elk
 docker-compose  down
+cd ../
+docker-compose -f docker-compose-jenkins.yaml down
 #docker-compose down
-#docker-compose -f docker-compose-elk.yml down
 
 #run elk stack
 #docker-compose -f docker-compose-elk.yml up &
+cd docker_elk
 docker-compose up -d 
 #docker run -d starlord345/filebeatimage:latest
 sleep 20
 
 #run jenkins
-#docker-compose up &
+cd ../
+docker-compose -f docker-compose-jenkins.yaml up -d
