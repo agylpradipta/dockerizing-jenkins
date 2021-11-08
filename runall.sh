bb76674d48c6f31d6caef4bb36e2ceca2b91a939
agylpradipta <agylpradipta70@gmail.com>
2021-11-08 09:56:58 +0700

    fix jenkins

--- runall.sh
+++ runall.sh
@@ -7,17 +7,17 @@ docker build -t filebeatimage .
 #clean anything with same name to get rid of clashes
 cd ../docker_elk
 docker-compose  down
-cd ../
-docker-compose -f docker-compose-jenkins.yaml down
+#cd ../
+#docker-compose -f docker-compose-jenkins.yaml down
 #docker-compose down
 
 #run elk stack
 #docker-compose -f docker-compose-elk.yml up &
-cd docker_elk
+#cd docker_elk
 docker-compose up -d 
 #docker run -d starlord345/filebeatimage:latest
 sleep 20
 
 #run jenkins
-cd ../
-docker-compose -f docker-compose-jenkins.yaml up -d
+#cd ../
+#docker-compose -f docker-compose-jenkins.yaml up -d
